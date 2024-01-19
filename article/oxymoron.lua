e9d9d7fe7229acbc719495ca7b5960b7eea35af9
beniza <beniza@gmail.com>
2024-01-19 17:51:56 +0530

    add final chapters

---
+++ article/oxymoron.lua
@@ -0,0 +1,38 @@
+
+“This statement is false.” 
+--
+If that sentence is true, and the statement is false, then the sentence is true. Even as it’s false.
+
+---
+“I must be cruel to be kind.” --Hamlet
+--
+He’s foreshadowing his plans to avenge his father’s murder by killing his father’s murderer Claudius, who is both Hamlet’s uncle and stepfather.
+--
+Hamlet knows this will hurt his mother, now married to Claudius. She’s going to lose a second husband.
+--
+In Hamlet’s mind, however, it’s kinder to kill Claudius and avenge his father’s death, rather than let his mother remain married to the man who murdered her husband.
+
+---
+“All animals are created equal, but some are more equal than others.” --George Orwell’s Animal Farm
+--
+Orwell uses paradox to make a political statement about Stalinism and the Soviet Union.
+
+---
+“Me, I always tell the truth. Even when I lie.” --Scarface
+--
+A less mind-bending example of a paradox is from the movie Scarface.
+--
+Tony’s greed and ambition may lead him to do a lot of things, but he doesn’t hide the lengths he’ll go to get what he wants. You always know who Tony is and what he is willing to do.
+
+But others who were equally greedy and ambitious hide behind the veneer of politeness. You think you’re safe with them. But are you? At least with Tony, you know you need to watch your back.
+
+---
+“Life is much too important to be taken seriously.” --Oscar Wilde
+--
+Wilde wanted to remind people it’s important for them to enjoy their lives. People should avoid taking themselves too seriously.
+
+---
+“The only thing we have to fear is fear itself.” --Roosevelt
+--
+At Franklin D. Roosevelt’s Inauguration, he wanted his listeners to understand that the more afraid you are of something, the more power it has over you. Don’t let fear keep you from taking action.
+
